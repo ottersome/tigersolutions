c02b543ccc849b23529711a72cea8bc74b7c6b33
Luis Garcia <luisgarcia26284@gmail.com>
2021-07-02 14:39:52 +0800

    Adding testo

--- scraper.py
+++ scraper.py
@@ -1,15 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
+import geograpy
+import nltk
 
 listOfWebsites = [\
-        'ZZ',\
         ]
 
 class Scraper:
     def __init__(curLocation, symptoms):
         self.curLocation = curLocation
         self.symptoms = symptoms
-    def 
+        nltk.download('punkt')
+        nltk.download('averaged_perceptron_tagger')
+        nltk.download('maxent_ne_chunker')
+        nltk.download('words')
+    def getUnfilteredArticles(mainSource):
+        for source in mainSources:
+            pass
 
 URL = "https://realpython.github.io/fake-jobs/"
 page = requests.get(URL)
